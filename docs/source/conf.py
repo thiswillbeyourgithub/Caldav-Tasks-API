f5fc4e36223c864555ed066a2e8e14cc6d63a0cd
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-05-15 16:31:36 +0200

    wrong theme argument
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,6 @@ source_suffix = {
 
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-    "flyout_display": "attached",
     "language_selector": False,
     "style_external_links": True,
     "prev_next_buttons_location": "both",
