8385cee101b75d073d4310facb1882d987e6d124
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-10-06 03:57:14 +0200

    bump version 1.4.0 -> 1.5.0

--- caldav_tasks_api/caldav_tasks_api.py
+++ caldav_tasks_api/caldav_tasks_api.py
@@ -17,7 +17,7 @@ from caldav_tasks_api.utils.data import (
 
 class TasksAPI:
 
-    VERSION: str = "1.4.0"
+    VERSION: str = "1.5.0"
 
     def __init__(
         self,
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="caldav-tasks-api",
-    version="1.4.0",
+    version="1.5.0",
     author="thiswillbeyourgithub",
     description="A Python client for CalDAV task servers, with a CLI.",
     long_description=open("README.md").read(),
